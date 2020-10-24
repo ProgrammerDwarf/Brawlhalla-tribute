39448e404ec290bd284baf6cd79cc2474473543c
Reinid Valarino <reinidval@gmail.com>
2020-10-24 15:37:38 -0300

    Change the color palette and layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,15 @@
-img = document.getElementById('modal');
-console.log(img);
+imgSquare = document.querySelectorAll(".polaroid");
+
+
+imgSquare.forEach(function(squares){
+    squares.addEventListener("click", function(ev) {
+        ev.preventDefault();
+
+        let idImg = document.getElementsByClassName("polaroid");
+        console.log(idImg);
+        
+        let ImgId = idImg[0];
+        console.log(ImgId);
+    })
+} 
+);
